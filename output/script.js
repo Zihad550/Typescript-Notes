e0c69f5e2595c557ae72d5fc980604ca5bf7df50
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-31 07:55:24 +0600

    note-3 updated

--- output/script.js
+++ output/script.js
@@ -1,3 +1,40 @@
 "use strict";
-const country = "i love bangladesh";
-console.log(country);
+// here ts has autometically setted the type of first name variable
+let firstName = "jehad";
+//! you cannot set this value number
+// firstName = 18;
+// but you do this
+let lastName;
+lastName = "Hussain";
+lastName = 18;
+// now here is no error because it only sets the type automatically when you declaire and set the variable value at one time
+// function
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(2, 2));
+// object and array
+let fruits = ["apple", "banana", "orange"];
+// you cannot push number because fruits is string array
+fruits.push("mango");
+// fruits.push(34);
+// autometically sets type
+const mixed = ["person", 18, false];
+mixed.push(true);
+mixed.push("name");
+mixed.push(15);
+// for object
+const person = {
+    name: "jehad",
+    age: 18,
+    isStudent: true,
+};
+// because it sets that the current person object is its schema you cannot add else property that does not exists on person object
+//todo: person.old = 18;
+// you can do this
+person.name = "Rabbi";
+// but if you try to do this
+//todo: person.name = 18;
+// it will give you error
+// because it automatically sets the type if you don't tell it
+// it is also true for other element
--- readme.md
+++ readme.md
@@ -1,15 +1,5 @@
-# Note-2
+# Note-3
 
-## setup typescript for project
+## automatically typed
 
-- tsc --init = to initialize a typescript project
-- go to tsconfig.json file
-- rootDir = where your ts file exists
-- outDir = where you want all your js files to exist
-- now you just write tsc = it will find the files automatically on the root dir
-- tsc -w = now it will look if there are any change on ts file and automatically compile to js file
-- you don't have to write tsc again and again
-
-**for a specific to compile**
-
-- tsc file-name = to compile ts into js
+- when you do declaire and assign together then it will autometically set the type.
