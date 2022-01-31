65c7984dc67b3cc3d3d387a2424e814f78638e4f
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-31 12:34:31 +0600

    note-12 updated

--- output/script.js
+++ output/script.js
@@ -1,19 +1,14 @@
 "use strict";
-const addId = (obj) => {
-    let id = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { id });
-};
-let user = addId({
-    name: "Jehad",
-    age: 40,
-    country: "bangladesh",
-});
-console.log(user.id);
-console.log(user.name);
-console.log(user.country);
+var RType;
+(function (RType) {
+    RType[RType["SUCCESS"] = 0] = "SUCCESS";
+    RType[RType["FAILURE"] = 1] = "FAILURE";
+    RType[RType["UNAUTHENTICATED"] = 2] = "UNAUTHENTICATED";
+    RType[RType["FORBIDDEN"] = 3] = "FORBIDDEN";
+})(RType || (RType = {}));
 const response1 = {
     status: 200,
-    type: "Good",
+    type: RType.FAILURE,
     data: {
         name: "Jehad",
         something: 300,
@@ -21,6 +16,8 @@ const response1 = {
 };
 const response2 = {
     status: 200,
-    type: "Great",
+    type: RType.SUCCESS,
     data: "Error",
 };
+console.log(response1);
+console.log(response2);
