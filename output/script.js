58a695d4f0204f0b409b0e411d80483a1be4bb95
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-31 09:16:55 +0600

    note-6 updated

--- output/script.js
+++ output/script.js
@@ -5,5 +5,6 @@ myFunc = () => {
 };
 const anotherFunc = (a, b, c, d = 18) => {
     console.log(`${a} is ${b} years old `);
+    return a;
 };
 anotherFunc("Jehad", 18);
--- src/script.ts
+++ src/script.ts
@@ -6,8 +6,17 @@ myFunc = () => {
 
 // here c is a optional parameter
 // d has assigned default value
-const anotherFunc = (a: string, b: number, c?: string, d: number = 18) => {
+// todo: we can tell the return type after all the parameters
+const anotherFunc = (
+  a: string,
+  b: number,
+  c?: string,
+  d: number = 18
+  // return type = string
+): string => {
   console.log(`${a} is ${b} years old `);
+  //** by default functions in Ts returns void */
+  return a;
 };
 // here jehad and 18 are arguments
 anotherFunc("Jehad", 18);
